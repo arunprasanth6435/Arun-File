78fd6671534229d1447367dc0a3b78d40f4a568e
Arunprasanth T <arunprasanth6435@gmail.com>
2026-03-25 20:56:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Arunprasanth"
+course = "Student Project"
+print("Welcome to My First GitHub Project!")
+print("Name:", name)
+print("Course:", course)
+print("\nSimple Addition Program")
+a = 10
+b = 5
+sum = a + b
+print("Addition of", a, "and", b, "is:", sum)
+print("\nThank you for visiting my project!")
